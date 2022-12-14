5d9ed7041452d29f72786abd3340a4347d698a74
Đỗ Thịnh <thinhxt2000@gmail.com>
2022-12-14 12:36:24 +0700

    'a'

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,12 +2,25 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 
 @SpringBootApplication
 
 public class Application {
+	@Bean
+	public WebMvcConfigurer configurer() {
+		return new WebMvcConfigurer() {
+			
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("*");
+			}
+		};
+	}
 
        public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/example/SpringFoxConfig.java
+++ src/main/java/com/example/SpringFoxConfig.java
@@ -2,8 +2,6 @@ package com.example;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,10 +9,16 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SpringFoxConfig implements WebMvcConfigurer {
-	
-	public void addCorsMapping(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("*");
-	}
+public class SpringFoxConfig {
+	@Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.api"))
+                .paths(PathSelectors.any())
+                .build()
+                ;
+    }
+
     
 }
